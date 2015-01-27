d293016a8adc50085e5b69040f721367f7939f7a
Anjana <anjanav@coli.uni-saarland.de>
2015-01-27 16:04:13 +0100

    Added weka as a dependency

--- grails/destressapp/grails-app/conf/BuildConfig.groovy
+++ grails/destressapp/grails-app/conf/BuildConfig.groovy
@@ -52,6 +52,7 @@ grails.project.dependency.resolution = {
         // runtime 'org.postgresql:postgresql:9.3-1101-jdbc41'
         test "org.grails:grails-datastore-test-support:1.0.2-grails-2.4"
 		runtime "com.google.guava:guava:13.0.1"
+		compile 'nz.ac.waikato.cms.weka:weka-stable:3.6.12'
     }
 
     plugins {
