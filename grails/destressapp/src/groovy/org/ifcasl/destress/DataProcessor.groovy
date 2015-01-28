da8d70d712be2c0e208176b820bfbad38ee9f697
Anjana <anjanav@coli.uni-saarland.de>
2015-01-28 14:47:48 +0100

    CSV I/O is now wicked simple thanks to Weka - #11

--- grails/destressapp/src/groovy/org/ifcasl/destress/DataProcessor.groovy
+++ grails/destressapp/src/groovy/org/ifcasl/destress/DataProcessor.groovy
@@ -1,29 +1,25 @@
 package org.ifcasl.destress
 
-import org.grails.plugins.csv.CSVMapReader
-import org.grails.plugins.csv.CSVWriter
+//import org.grails.plugins.csv.CSVMapReader
+//import org.grails.plugins.csv.CSVWriter
 
-import weka.core.Attribute;
-import weka.core.FastVector;
-import weka.core.Instance;
-import weka.core.Instances;
+import weka.core.Attribute
+import weka.core.FastVector
+import weka.core.Instance
+import weka.core.Instances
+import weka.core.converters.ArffSaver
+import weka.core.converters.CSVLoader
 
 
 class DataProcessor {
 	
 	String inputCsv
-	String outputFile
-	//CSVMapReader mapReader
-	//CSVWriter writer
-	List mapList
-	Set columns
-	
+	Instances data
+		
 
 	public DataProcessor(String inputCsvPath) {
 		this.inputCsv = inputCsvPath
-		def mapReader = new File(inputCsvPath).toCsvMapReader()
-		this.mapList = mapReader.toList()
-		this.columns = this.mapList[0].keySet()
+		this.data = loadCsvWeka()
 	}
 	
 	public String toString() {
@@ -31,10 +27,16 @@ class DataProcessor {
 		output += this.inputCsv 
 		//output += " - " 
 		//output += this.mapReader.toList()[0]
-		output += "</p><p>"	
-		output += this.columns.toString()
-		output += "</p>"
+		output += "</p>"	
+		output += "<p>Header of dataset:</p>"
+		output += (new Instances(this.data, 0)).toString();
 		return output
+	}
+	
+	public Instances loadCsvWeka() {
+		CSVLoader loader = new CSVLoader();
+		loader.setSource(new File(this.inputCsv));
+		return loader.getDataSet();
 	}
 
 }
