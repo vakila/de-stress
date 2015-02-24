fa060e7d7c39bab2142f5f5f4335763ad1bc126f
Anjana <anjanav@coli.uni-saarland.de>
2015-02-24 23:14:19 +0100

    FeatureExtractor is passing all tests! (#16, #17, #26)

--- grails/destressapp/test/unit/org/ifcasl/destress/FeatureExtractorTest.groovy
+++ grails/destressapp/test/unit/org/ifcasl/destress/FeatureExtractorTest.groovy
@@ -23,23 +23,34 @@ class FeatureExtractorTest extends GroovyTestCase {
 	}
 
 	
-	public void test() {
+	public void testDuration() {
+		//Word
 		assert featex.getWordDuration() > 0
 		println "wordDuration: " + featex.getWordDuration()
 		assert featex.getVowelDurationInWord() > 0
 		println "vowelDurationInWord: " + featex.getVowelDurationInWord()
 		
+		//Syllables
 		assert featex.getSyllableDuration(0) > 0
 		println "syllableDuration(0): " + featex.getSyllableDuration(0)
 		assert featex.getSyllableDuration(1) > 0
 		println "syllableDuration(1): " + featex.getSyllableDuration(1)
 		
-		
+		//Vowels
 		assert featex.getVowelDurationInSyllable(0) > 0
 		println "vowelDurationInSyllable(0): " + featex.getVowelDurationInSyllable(0)
 		assert featex.getVowelDurationInSyllable(1) > 0
 		println "vowelDurationInSyllable(1): " + featex.getVowelDurationInSyllable(1)
 		
+		assert featex.getRelSyllDuration() > 0
+		println "relSyllDuration: " + featex.getRelSyllDuration()
+		assert featex.getRelVowelDuration() > 0
+		println "relVowelDuration: " + featex.getRelVowelDuration()
+	}
+	
+	public void testF0() {
+		
+		//Word
 		assert featex.getWordF0Mean() > 0 
 		println "wordF0Mean: " + featex.getWordF0Mean()
 		assert featex.getWordF0Max() > 0
@@ -49,6 +60,7 @@ class FeatureExtractorTest extends GroovyTestCase {
 		assert featex.getWordF0Range() > 0
 		println "wordF0Range: " + featex.getWordF0Range()
 		
+		//Syllables
 		assert featex.getSyllableF0Mean(0) > 0
 		assert featex.getSyllableF0Max(0) > 0
 		assert featex.getSyllableF0Min(0) > 0
@@ -57,7 +69,6 @@ class FeatureExtractorTest extends GroovyTestCase {
 		println "syllableF0Max(0): " + featex.getSyllableF0Max(0)
 		println "syllableF0Min(0): " + featex.getSyllableF0Min(0)
 		println "syllableF0Range(0): " + featex.getSyllableF0Range(0)
-		
 		assert featex.getSyllableF0Mean(1) > 0
 		assert featex.getSyllableF0Max(1) > 0
 		assert featex.getSyllableF0Min(1) > 0
@@ -67,6 +78,7 @@ class FeatureExtractorTest extends GroovyTestCase {
 		println "syllableF0Min(1): " + featex.getSyllableF0Min(1)
 		println "syllableF0Range(1): " + featex.getSyllableF0Range(1)
 		
+		//Vowels
 		assert featex.getVowelF0Mean(0) > 0
 		assert featex.getVowelF0Max(0) > 0
 		assert featex.getVowelF0Min(0) > 0
@@ -75,7 +87,6 @@ class FeatureExtractorTest extends GroovyTestCase {
 		println "vowelF0Max(0): " + featex.getVowelF0Max(0)
 		println "vowelF0Min(0): " + featex.getVowelF0Min(0)
 		println "vowelF0Range(0): " + featex.getVowelF0Range(0)
-		
 		assert featex.getVowelF0Mean(1) > 0
 		assert featex.getVowelF0Max(1) > 0
 		assert featex.getVowelF0Min(1) > 0
@@ -85,12 +96,71 @@ class FeatureExtractorTest extends GroovyTestCase {
 		println "vowelF0Min(1): " + featex.getVowelF0Min(1)
 		println "vowelF0Range(1): " + featex.getVowelF0Range(1)
 		
+		//Relative
+		assert featex.getRelSyllF0Mean() > 0
+		assert featex.getRelSyllF0Max() > 0
+		assert featex.getRelSyllF0Min() > 0
+		assert featex.getRelSyllF0Range() > 0
+		println "relSyllF0Mean: " + featex.getRelSyllF0Mean()
+		println "relSyllF0Max: " + featex.getRelSyllF0Max()
+		println "relSyllF0Min: " + featex.getRelSyllF0Min()
+		println "relSyllF0Range: " + featex.getRelSyllF0Range()
+		assert featex.getRelVowelF0Mean() > 0
+		assert featex.getRelVowelF0Max() > 0
+		assert featex.getRelVowelF0Min() > 0
+		assert featex.getRelVowelF0Range() > 0
+		println "relVowelF0Mean: " + featex.getRelVowelF0Mean()
+		println "relVowelF0Max: " + featex.getRelVowelF0Max()
+		println "relVowelF0Min: " + featex.getRelVowelF0Min()
+		println "relVowelF0Range: " + featex.getRelVowelF0Range()
 		assertNotNull(featex.getMaxF0Index())
 		println "maxF0Index: " + featex.getMaxF0Index()
 		assertNotNull(featex.getMinF0Index())
 		println "minF0Index: " + featex.getMinF0Index()
 		assertNotNull(featex.getMaxRangeF0Index())
 		println "maxRangeF0Index: " + featex.getMaxRangeF0Index()
+		
+	}
+	
+	public void testEnergy() {
+		
+		//Word
+		assert featex.getWordEnergyMean() > 0
+		println "wordEnergyMean: " + featex.getWordEnergyMean()
+		assert featex.getWordEnergyMax() > 0
+		println "wordEnergyMax: " + featex.getWordEnergyMax()
+		
+		//Syllables
+		assert featex.getSyllableEnergyMean(0) > 0
+		assert featex.getSyllableEnergyMax(0) > 0
+		println "syllableEnergyMean(0): " + featex.getSyllableEnergyMean(0)
+		println "syllableEnergyMax(0): " + featex.getSyllableEnergyMax(0)
+		assert featex.getSyllableEnergyMean(1) > 0
+		assert featex.getSyllableEnergyMax(1) > 0
+		println "syllableEnergyMean(1): " + featex.getSyllableEnergyMean(1)
+		println "syllableEnergyMax(1): " + featex.getSyllableEnergyMax(1)
+		
+		//Vowels
+		assert featex.getVowelEnergyMean(0) > 0
+		assert featex.getVowelEnergyMax(0) > 0
+		println "vowelEnergyMean(0): " + featex.getVowelEnergyMean(0)
+		println "vowelEnergyMax(0): " + featex.getVowelEnergyMax(0)
+		assert featex.getVowelEnergyMean(1) > 0
+		assert featex.getVowelEnergyMax(1) > 0
+		println "vowelEnergyMean(1): " + featex.getVowelEnergyMean(1)
+		println "vowelEnergyMax(1): " + featex.getVowelEnergyMax(1)
+		
+		//Relative
+		assert featex.getRelSyllEnergyMean() > 0
+		assert featex.getRelSyllEnergyMax() > 0
+		println "relSyllEnergyMean: " + featex.getRelSyllEnergyMean()
+		println "relSyllEnergyMax: " + featex.getRelSyllEnergyMax()
+		assert featex.getRelVowelEnergyMean() > 0
+		assert featex.getRelVowelEnergyMax() > 0
+		println "relVowelEnergyMean: " + featex.getRelVowelEnergyMean()
+		println "relVowelEnergyMax: " + featex.getRelVowelEnergyMax()
+		assertNotNull(featex.getMaxEnergyIndex())
+		println "maxEnergyIndex: " + featex.getMaxEnergyIndex()
 		
 	}
 	
