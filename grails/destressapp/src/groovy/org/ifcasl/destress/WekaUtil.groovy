9ad2168a7a74b8e16ba6bf374879286c66f83b19
Anjana <anjanav@coli.uni-saarland.de>
2015-03-18 12:35:28 +0100

    WekaUtil.getInstance(wordUtt) (#31)

--- grails/destressapp/src/groovy/org/ifcasl/destress/WekaUtil.groovy
+++ grails/destressapp/src/groovy/org/ifcasl/destress/WekaUtil.groovy
@@ -39,15 +39,13 @@ class WekaUtil {
         Instance inst = new Instance(nAtts)
         inst.setDataset(DATASET)
 
-
         FeatureExtractor featex =  new FeatureExtractor(wordUtt.sentenceUtterance.waveFile,
                                                         wordUtt.sentenceUtterance.gridFile,
                                                         wordUtt.word.text)
 
         //println("Equal headers? " + inst.equalHeaders(DATASET.instance(57)))
-        println("Instance 57:")
-        println(DATASET.instance(57).toString())
-
+        // println("Instance 57:")
+        // println(DATASET.instance(57).toString())
 
         // for (att in DATASET.enumerateAttributes()) {
         //     println att.name()
@@ -63,7 +61,6 @@ class WekaUtil {
         inst.setValue(DATASET.attribute("SPEAKER_GENDER"), wordUtt.sentenceUtterance.speaker.ageGender.toString())
         inst.setValue(DATASET.attribute("SPEAKER_LEVEL"), wordUtt.sentenceUtterance.speaker.skillLevel.toString())
 
-
         //// DURATION
         inst.setValue(DATASET.attribute("WORD_DUR"), featex.getWordDuration())
         inst.setValue(DATASET.attribute("SYLL0_DUR"), featex.getSyllableDuration(0))
@@ -72,7 +69,6 @@ class WekaUtil {
         inst.setValue(DATASET.attribute("V1_DUR"), featex.getVowelDurationInSyllable(0))
         inst.setValue(DATASET.attribute("REL_SYLL_DUR"), featex.getVowelDurationInSyllable(1))
         inst.setValue(DATASET.attribute("REL_V_DUR"), featex.getRelVowelDuration())
-
 
         //// F0
         // Word
@@ -89,82 +85,48 @@ class WekaUtil {
         inst.setValue(DATASET.attribute("SYLL1_F0_MAX"), featex.getSyllableF0Max(1))
         inst.setValue(DATASET.attribute("SYLL1_F0_MIN"), featex.getSyllableF0Min(1))
         inst.setValue(DATASET.attribute("SYLL1_F0_RANGE"), featex.getSyllableF0Range(1))
-        // // Vowels
-        // inst.setValue(V0_F0_MEAN, featex.getVowelF0Mean(0))
-        // inst.setValue(V0_F0_MAX, featex.getVowelF0Max(0))
-        // inst.setValue(V0_F0_MIN, featex.getVowelF0Min(0))
-        // inst.setValue(V0_F0_RANGE, featex.getVowelF0Range(0))
-        // inst.setValue(V1_F0_MEAN, featex.getVowelF0Mean(1))
-        // inst.setValue(V1_F0_MAX, featex.getVowelF0Max(1))
-        // inst.setValue(V1_F0_MIN, featex.getVowelF0Min(1))
-        // inst.setValue(V1_F0_RANGE, featex.getVowelF0Range(1))
-        // // inst.setValue(DATASET.attribute("V0_F0_MEAN"), )
-        // // inst.setValue(DATASET.attribute("V0_F0_MAX"), )
-        // // inst.setValue(DATASET.attribute("V0_F0_MIN"), )
-        // // inst.setValue(DATASET.attribute("V0_F0_RANGE"), )
-        // // inst.setValue(DATASET.attribute("V1_F0_MEAN"), )
-        // // inst.setValue(DATASET.attribute("V1_F0_MAX"), )
-        // // inst.setValue(DATASET.attribute("V1_F0_MIN"), )
-        // // inst.setValue(DATASET.attribute("V1_F0_RANGE"), )
-        // // Relative
-        // inst.setValue(REL_SYLL_F0_MEAN, featex.getRelSyllF0Mean())
-        // inst.setValue(REL_SYLL_F0_MAX, featex.getRelSyllF0Max())
-        // inst.setValue(REL_SYLL_F0_MIN, featex.getRelSyllF0Min())
-        // inst.setValue(REL_SYLL_F0_RANGE, featex.getRelSyllF0Range())
-        // inst.setValue(REL_VOWEL_F0_MEAN, featex.getRelVowelF0Mean())
-        // inst.setValue(REL_VOWEL_F0_MAX, featex.getRelVowelF0Max())
-        // inst.setValue(REL_VOWEL_F0_MIN, featex.getRelVowelF0Min())
-        // inst.setValue(REL_VOWEL_F0_RANGE, featex.getRelVowelF0Range())
-        // inst.setValue(F0_MAX_INDEX, featex.getMaxF0Index())
-        // inst.setValue(F0_MIN_INDEX, featex.getMinF0Index())
-        // inst.setValue(F0_MAXRANGE_INDEX, featex.getMaxRangeF0Index())
-        // // inst.setValue(DATASET.attribute("REL_SYLL_F0_MEAN"), )
-        // // inst.setValue(DATASET.attribute("REL_SYLL_F0_MAX"), )
-        // // inst.setValue(DATASET.attribute("REL_SYLL_F0_MIN"), )
-        // // inst.setValue(DATASET.attribute("REL_SYLL_F0_RANGE"), )
-        // // inst.setValue(DATASET.attribute("REL_VOWEL_F0_MEAN"), )
-        // // inst.setValue(DATASET.attribute("REL_VOWEL_F0_MAX"), )
-        // // inst.setValue(DATASET.attribute("REL_VOWEL_F0_MIN"), )
-        // // inst.setValue(DATASET.attribute("REL_VOWEL_F0_RANGE"), )
-        // // inst.setValue(DATASET.attribute("F0_MAX_INDEX"), )
-        // // inst.setValue(DATASET.attribute("F0_MIN_INDEX"), )
-        // // inst.setValue(DATASET.attribute("F0_MAXRANGE_INDEX"), )
-        //
-        // //// ENERGY
-        // //Word
-        // inst.setValue(WORD_ENERGY_MEAN, featex.getWordEnergyMean())
-        // inst.setValue(WORD_ENERGY_MAX, featex.getWordEnergyMax())
-        // // inst.setValue(DATASET.attribute("WORD_ENERGY_MEAN"), )
-        // // inst.setValue(DATASET.attribute("WORD_ENERGY_MAX"), )
-        // //Syllables
-        // inst.setValue(SYLL0_ENERGY_MEAN, featex.getSyllableEnergyMean(0))
-        // inst.setValue(SYLL0_ENERGY_MAX, featex.getSyllableEnergyMax(0))
-        // inst.setValue(SYLL1_ENERGY_MEAN, featex.getSyllableEnergyMean(1))
-        // inst.setValue(SYLL1_ENERGY_MAX, featex.getSyllableEnergyMax(1))
-        // // inst.setValue(DATASET.attribute("SYLL0_ENERGY_MEAN"), )
-        // // inst.setValue(DATASET.attribute("SYLL0_ENERGY_MAX"), )
-        // // inst.setValue(DATASET.attribute("SYLL1_ENERGY_MEAN"), )
-        // // inst.setValue(DATASET.attribute("SYLL1_ENERGY_MAX"), )
-        // //Vowels
-        // inst.setValue(V0_ENERGY_MEAN, featex.getVowelEnergyMean(0))
-        // inst.setValue(V0_ENERGY_MAX, featex.getVowelEnergyMax(0))
-        // inst.setValue(V1_ENERGY_MEAN, featex.getVowelEnergyMean(1))
-        // inst.setValue(V1_ENERGY_MAX, featex.getVowelEnergyMax(1))
-        // // inst.setValue(DATASET.attribute("V0_ENERGY_MEAN"), )
-        // // inst.setValue(DATASET.attribute("V0_ENERGY_MAX"), )
-        // // inst.setValue(DATASET.attribute("V1_ENERGY_MEAN"), )
-        // // inst.setValue(DATASET.attribute("V1_ENERGY_MAX"), )
-        // //Relative
-        // inst.setValue(REL_SYLL_ENERGY_MEAN, featex.getRelSyllEnergyMean())
-        // inst.setValue(REL_SYLL_ENERGY_MAX, featex.getRelSyllEnergyMax())
-        // inst.setValue(REL_VOWEL_ENERGY_MEAN, featex.getRelVowelEnergyMean())
-        // inst.setValue(REL_VOWEL_ENERGY_MAX, featex.getRelVowelEnergyMax())
-        // inst.setValue(ENERGY_MAX_INDEX, featex.getMaxEnergyIndex())
-        // // inst.setValue(DATASET.attribute("REL_SYLL_ENERGY_MEAN"), )
-        // // inst.setValue(DATASET.attribute("REL_SYLL_ENERGY_MAX"), )
-        // // inst.setValue(DATASET.attribute("REL_VOWEL_ENERGY_MEAN"), )
-        // // inst.setValue(DATASET.attribute("REL_VOWEL_ENERGY_MAX"), )
-        // // inst.setValue(DATASET.attribute("ENERGY_MAX_INDEX"), )
+        // Vowels
+        inst.setValue(DATASET.attribute("V0_F0_MEAN"), featex.getVowelF0Mean(0))
+        inst.setValue(DATASET.attribute("V0_F0_MAX"), featex.getVowelF0Max(0))
+        inst.setValue(DATASET.attribute("V0_F0_MIN"), featex.getVowelF0Min(0))
+        inst.setValue(DATASET.attribute("V0_F0_RANGE"), featex.getVowelF0Range(0))
+        inst.setValue(DATASET.attribute("V1_F0_MEAN"), featex.getVowelF0Mean(1))
+        inst.setValue(DATASET.attribute("V1_F0_MAX"), featex.getVowelF0Max(1))
+        inst.setValue(DATASET.attribute("V1_F0_MIN"), featex.getVowelF0Min(1))
+        inst.setValue(DATASET.attribute("V1_F0_RANGE"), featex.getVowelF0Range(1))
+        // Relative
+        inst.setValue(DATASET.attribute("REL_SYLL_F0_MEAN"), featex.getRelSyllF0Mean())
+        inst.setValue(DATASET.attribute("REL_SYLL_F0_MAX"), featex.getRelSyllF0Max())
+        inst.setValue(DATASET.attribute("REL_SYLL_F0_MIN"), featex.getRelSyllF0Min())
+        inst.setValue(DATASET.attribute("REL_SYLL_F0_RANGE"), featex.getRelSyllF0Range())
+        inst.setValue(DATASET.attribute("REL_VOWEL_F0_MEAN"), featex.getRelVowelF0Mean())
+        inst.setValue(DATASET.attribute("REL_VOWEL_F0_MAX"), featex.getRelVowelF0Max())
+        inst.setValue(DATASET.attribute("REL_VOWEL_F0_MIN"), featex.getRelVowelF0Min())
+        inst.setValue(DATASET.attribute("REL_VOWEL_F0_RANGE"), featex.getRelVowelF0Range())
+        inst.setValue(DATASET.attribute("F0_MAX_INDEX"), featex.getMaxF0Index())
+        inst.setValue(DATASET.attribute("F0_MIN_INDEX"), featex.getMinF0Index())
+        inst.setValue(DATASET.attribute("F0_MAXRANGE_INDEX"), featex.getMaxRangeF0Index())
+
+        //// ENERGY
+        //Word
+        inst.setValue(DATASET.attribute("WORD_ENERGY_MEAN"), featex.getWordEnergyMean())
+        inst.setValue(DATASET.attribute("WORD_ENERGY_MAX"), featex.getWordEnergyMax())
+        //Syllables
+        inst.setValue(DATASET.attribute("SYLL0_ENERGY_MEAN"), featex.getSyllableEnergyMean(0))
+        inst.setValue(DATASET.attribute("SYLL0_ENERGY_MAX"), featex.getSyllableEnergyMax(0))
+        inst.setValue(DATASET.attribute("SYLL1_ENERGY_MEAN"), featex.getSyllableEnergyMean(1))
+        inst.setValue(DATASET.attribute("SYLL1_ENERGY_MAX"), featex.getSyllableEnergyMax(1))
+        //Vowels
+        inst.setValue(DATASET.attribute("V0_ENERGY_MEAN"), featex.getVowelEnergyMean(0))
+        inst.setValue(DATASET.attribute("V0_ENERGY_MAX"), featex.getVowelEnergyMax(0))
+        inst.setValue(DATASET.attribute("V1_ENERGY_MEAN"), featex.getVowelEnergyMean(1))
+        inst.setValue(DATASET.attribute("V1_ENERGY_MAX"), featex.getVowelEnergyMax(1))
+        //Relative
+        inst.setValue(DATASET.attribute("REL_SYLL_ENERGY_MEAN"), featex.getRelSyllEnergyMean())
+        inst.setValue(DATASET.attribute("REL_SYLL_ENERGY_MAX"), featex.getRelSyllEnergyMax())
+        inst.setValue(DATASET.attribute("REL_VOWEL_ENERGY_MEAN"), featex.getRelVowelEnergyMean())
+        inst.setValue(DATASET.attribute("REL_VOWEL_ENERGY_MAX"), featex.getRelVowelEnergyMax())
+        inst.setValue(DATASET.attribute("ENERGY_MAX_INDEX"), featex.getMaxEnergyIndex())
 
         //// CLASS
         //inst.setValue(DATASET.attribute("STRESS"), )
@@ -176,26 +138,7 @@ class WekaUtil {
 
     }
 
-    ///// MOVED TO classifierScript.groovy
-    // public static FilteredClassifier getClassifier() {
-    //     DATASET.setClassIndex(DATASET.numAttributes() - 1)
-    //
-    //     Remove thisFilter = (Remove) weka.core.SerializationHelper.read(FILTERFILE)
-    //     thisFilter.setInputFormat(DATASET)
-    //     thisFilter.setOutputFormat(DATASET)
-    //
-    //     def thisClassifier = new SimpleCart()
-    //
-    //     FilteredClassifier fc = new FilteredClassifier()
-    //     fc.setFilter(thisFilter)
-    //     fc.setClassifier(thisClassifier)
-    //
-    //     fc.buildClassifier(DATASET)
-    //
-    //     println("\nBuilt classifier:\n" + fc.toString())
-    //
-    //     return fc
-    // }
+
 
 
 }
