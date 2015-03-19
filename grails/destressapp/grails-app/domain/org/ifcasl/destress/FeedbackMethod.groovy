6a94687f1f8849ed8eb2b399e35b719526e166b9
Anjana <anjanav@coli.uni-saarland.de>
2015-03-19 23:32:04 +0100

    Constraints to mind FB/Diag compatibility (closes #39)

--- grails/destressapp/grails-app/domain/org/ifcasl/destress/FeedbackMethod.groovy
+++ grails/destressapp/grails-app/domain/org/ifcasl/destress/FeedbackMethod.groovy
@@ -21,8 +21,18 @@ class FeedbackMethod {
         name()
         description(blank:true,nullable:true)
         requiresScorerType(blank:true,nullable:true)
-        showSkillBars(blank:true,nullable:true)
-        playFeedbackSignal(blank:true,nullable:true)
+
+        showSkillBars(blank:true,nullable:true, validator: {val,obj ->
+            if (val==true && obj.requiresScorerType!=ScorerType.JSNOORI) {
+                return ['cannotShowBars', obj.requiresScorerType]
+            }
+            })
+        playFeedbackSignal(blank:true,nullable:true, validator: {val,obj ->
+            if (val==true && obj.requiresScorerType!=ScorerType.JSNOORI) {
+                return ['cannotPlayFeedback', obj.requiresScorerType]
+            }
+            })
+
         displayShapes(blank:true,nullable:true)
         styleText(blank:true,nullable:true)
         displayMessages(blank:true,nullable:true)
